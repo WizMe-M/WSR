cb907fb3af2b237b199968a78992276a560827c8
Timkin Maxim <enderman.2016@mail.ru>
2023-01-25 13:15:24 +0300

    auth view

---
+++ WSR.Desktop/Views/AuthView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WSR.Desktop.Views;
+
+public partial class AuthView : UserControl
+{
+    public AuthView()
+    {
+        InitializeComponent();
+    }
+}
